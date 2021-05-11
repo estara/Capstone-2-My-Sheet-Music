5cbf26b03a33f75fc18abbb443e23f4f8544c25c
Estara <heatherjpdx@gmail.com>
2021-05-11 10:08:12 -0700

    update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://my-sheet-music-backend.herokuapp.com/";
+const BASE_URL = "https://my-sheet-music-backend.herokuapp.com/";
 
 /** API Class.
  *
